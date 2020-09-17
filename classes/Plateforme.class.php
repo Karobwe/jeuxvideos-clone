3a9162c3dff94cc3359ccc3bc89878b41e41ce5d
Chanfi Attoumani <karobwe@netc.fr>
2020-09-17 17:08:41 +0200

    Ebauche de la classe Plateforme et son manager

--- classes/Plateforme.class.php
+++ classes/Plateforme.class.php
@@ -2,4 +2,45 @@
 
 class Plateforme {
     
+    /**
+     * @var int Identifiant dans la base de données
+     */
+    private int $idPlateforme;
+
+    /**
+     * @var string
+     */
+    private string $nomPlateforme;
+
+    /**
+     * @param array $datas
+     */
+    function __construct(array $datas) {
+        foreach ($datas as $key => $value) {
+            // On récupère le nom du setter correspondant à l'attribut.
+            $method = 'set'.ucfirst($key);
+            // Si le setter correspondant existe.
+            if (method_exists($this, $method)) {
+                // On appelle le setter.
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function getIdPlateforme(): int {
+        return $this->idPlateforme;
+    }
+
+    public function setIdPlateforme(int $idPlateforme): void {
+        $this->idPlateforme = $idPlateforme;
+    }
+
+    public function getNomPlateforme(): string {
+        return $this->nomPlateforme;
+    }
+
+    public function setNomPlateforme(string $nomPlateforme): void {
+        $this->nomPlateforme = $nomPlateforme;
+    }
+
 }
--- classes/PlateformeManager.class.php
+++ classes/PlateformeManager.class.php
@@ -1,5 +1,26 @@
 <?php
 
 class PlateformeManager {
+
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function add(Plateforme $plateforme) {
+        $stmt = $this->pdo->prepare("INSERT INTO `plateforme` (`idPlateforme`, `nomPlateforme`) 
+            VALUES (NULL, :nomPlateform); 
+        ");
+
+        $stmt->bindValue($plateforme->getNomPlateforme(), PDO::PARAM_STR);
+        $stmt->execute();
+        $stmt->closeCursor();
+
+        return $stmt->rowCount();
+    }
     
 }
