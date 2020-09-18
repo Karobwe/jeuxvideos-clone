a72868cd571aa9cf2ce27485087d3ffa9cfc2aca
Chanfi Attoumani <karobwe@netc.fr>
2020-09-18 09:09:10 +0200

    Ajout d'un exemple de fichier de config

---
+++ includes/config-sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Ceci est un fichier d'exemple des informations que doit contenir 
+ * le fichier "config.php", situé dans le dossier "/includes/".
+ */
+
+// Informations de connexion à la base de donnée
+
+define( 'DB_NAME', 'jeux_video' ); // Nom de la Base de données
+
+define( 'DB_USER', 'root' ); // Nom d'utilisateur
+
+define( 'DB_PASSWORD', '' ); // Mot de passe de l'utilisateur
+
+define( 'DB_HOST', 'localhost' ); // Hôte
